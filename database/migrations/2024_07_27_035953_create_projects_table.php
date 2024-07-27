6bfeafd18b17afefec136aacca169d58253ddae9
maulana19 <maulanahadi123456@gmail.com>
2024-07-27 11:17:08 +0700

    Add Table in database

---
+++ database/migrations/2024_07_27_035953_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->text('url');
+            $table->string('github');
+            $table->string('status');
+            $table->string('jenis');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
